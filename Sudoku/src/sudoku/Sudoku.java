c897d102b8cceec9bf9eb85ef794bd26328a8f2c
alberto <alberto@10.1.70.150>
2018-10-26 17:37:37 -0500

    Merge origin/master
    Conflicts:
    	Calculadora/src/calculadora/Calculadora.java

--- Sudoku/src/sudoku/Sudoku.java
+++ Sudoku/src/sudoku/Sudoku.java
@@ -4,7 +4,6 @@
  * and open the template in the editor.
  */
 package sudoku;
-import static sudoku.Validation.*;
 
 /**
  *
@@ -12,62 +11,237 @@ import static sudoku.Validation.*;
  */
 public class Sudoku {
     
-    private int[][] matrix;
+    private int[][] matriz, original;
+    private int [] primeraMod;
     
-    public Sudoku(){
-        this.matrix = new int[9][9];
+    public Sudoku(int[][] matriz){
+        this.matriz = matriz;
+        this.original = copia(matriz);
+        this.primeraMod = firstCell();
     }
     
-    public Sudoku(int[][] matrix) throws Exception {
-        setMatrix(matrix, false);
+    public void setElemento(int ren, int col, int newElement){
+        matriz[ren][col] = newElement;
     }
     
-    public Sudoku(int[][] matrix, boolean override) throws Exception{
-        setMatrix(matrix, override);
-    }
-    
-    public int[][] getMatrix(){
-        return matrix;
-    }
-    
-    public void setElement(int row, int col, int newElement){
-        matrix[row][col]=newElement;
-    }
-    
-    public void setMatrix(int[][] matrix, boolean override) throws Exception{
-        this.matrix = matrix;
-        for (int row=0; row<9; row++){
-            if (!(matrix.length==9 && matrix[row].length==9))
-                throw new Exception("WRONG MATRIX SIZE!!");
-            if (!override)
-                for (int col=0; col<9; col++)
-                    if (!valida(this,row,col))
-                        throw new Exception("INVALID NUMBERS!!");
-        }
-    }
-    
-    public Sudoku copy() throws Exception{
-        int[][] newmatrix = new int[9][9];
-        for(int row=0;row<9;row++)
-            System.arraycopy(matrix[row], 0, newmatrix[row], 0, 9);
-        Sudoku copy = new Sudoku(newmatrix, true);
-        return copy;
+    public int[][] copia(int [][] matriz){
+        int[][] nuevaMatriz = new int[9][9];
+        for(int ren = 0; ren<9; ren++)
+            System.arraycopy(matriz[ren], 0, nuevaMatriz[ren], 0, 9);
+        return nuevaMatriz;
     }
     
     @Override 
     public String toString(){
         StringBuilder cadena = new StringBuilder();
-        for (int row=0; row<9; row++){
-            if (row!=0)
+        for (int ren = 0; ren<9; ren++){
+            if (ren != 0)
                 cadena.append("\n");
-            for (int col=0; col<9; col++){
-                cadena.append(matrix[row][col]).append(" ");
-                if (col%3==2 && col!=8)
+            for(int col = 0; col<9; col++){
+                cadena.append(matriz[ren][col]).append(" ");
+                if(col%3 == 2 && col != 8)
                     cadena.append("| ");
             }
-            if (row%3==2 && row!=8)
+            if(ren%3 == 2 && ren != 8)
                 cadena.append("\n---------------------");
         }
         return cadena.toString();
     }
+    
+    public boolean validaRenglon(int ren){
+        boolean resp = true;
+        int col = 0;
+        ConjuntoA conjunto = new ConjuntoA();
+        while(col<9 && resp == true){
+            if ( matriz[ren][col] !=0 && conjunto.contiene(matriz[ren][col]))
+                resp = false;
+            else
+                conjunto.agrega(matriz[ren][col]);
+            col++;
+            }
+        return resp;
+    }
+    
+    public boolean validaColumna(int col){
+        boolean resp = true; 
+        int ren = 0;
+        ConjuntoA conjunto = new ConjuntoA();
+        while(ren<9 && resp == true){
+            if (matriz[ren][col] != 0 && conjunto.contiene(matriz[ren][col]))
+                resp = false;
+            conjunto.agrega(matriz[ren][col]);
+            ren++;
+            }
+        return resp;
+    }
+    
+    public boolean validaCuadricula(int ren, int col){
+        boolean resp = true; 
+        int i,maxI, j, maxJ;
+        maxI = ren-ren%3+3;
+        maxJ = col-col%3+3;
+        ConjuntoA conjunto = new ConjuntoA();
+        for (i = ren-ren%3; i<maxI; i++)
+            for(j = col-col%3; j<maxJ; j++){
+                if (matriz[i][j] != 0 && conjunto.contiene(matriz[i][j]))
+                    resp = false;
+                conjunto.agrega(matriz[i][j]);
+                }
+        return resp;
+        }
+    
+    public boolean valida(int ren, int col){
+        return validaRenglon(ren) &&
+               validaColumna(col) &&
+               validaCuadricula(ren, col);
+    }
+    
+    public int[] firstCell(){
+        boolean resp = false;
+        int[] res = new int[2];
+        int j;
+        int i = 0;
+        while(i<9 && resp == false){
+            j = 0;
+            while(j<9 && resp == false){
+                if (original[i][j] == 0 && resp == false){
+                    res[0] = i;
+                    res[1] = j;
+                    resp = true;
+                }
+                j++;
+            }
+            i++;
+        }
+        return res;
+    }
+    
+    public int[] prevCell(int ren, int col){
+        boolean resp = false;
+        int[] res = new int[2];
+        int a = -1, b =-1, j;
+        int i = 0;
+        while(i <= 8 && !resp){
+            j = 0;
+            while(j <= 8 && !resp){
+                if (i == ren && j == col)
+                        resp=true;
+                if (original[i][j] == 0 && !(i == ren && j == col)){
+                    a = i;
+                    b = j;
+                }
+                j++;
+            }
+            i++;
+        }
+        res[0] = a; res[1] = b;
+        return res;
+    }
+    
+    public int prevRen(int ren, int col){
+        return prevCell(ren, col)[0];
+    }
+    
+    public int prevCol(int ren, int col){
+        return prevCell(ren, col)[1];
+    }
+    
+    public int nextRen(int ren, int col){
+        if (col == 8 && ren != 8)
+            return ren+1;
+        else
+            return ren;
+    }
+    public int nextCol(int ren, int col){
+        if (col == 8 && ren == 8)
+            return col;
+        else if (col == 8)
+            return 0;
+        else
+            return col+1;
+    }
+    
+    public boolean resuelve(){ 
+        return resuelve(0, 0);
+    }
+    
+    private  boolean resuelve(int ren, int col){
+        //System.out.println(this.toString()+"\n");
+        // Estado base donde no se puede resolver el problema
+        if(ren == primeraMod[0] && col == primeraMod[1]
+           && matriz[ren][col] == 9 && !valida(ren,col))
+            return false;
+        // Estado base donde el problema ha sido resuelto
+        else if (ren == 8 && col == 8 && matriz[ren][col] != 0 && valida(ren, col))
+            return true;
+        // Si la celda no es modificable
+        else if (original[ren][col] != 0)
+            return resuelve(nextRen(ren,col), nextCol(ren,col));
+        // Si la celda es modificable
+        else {
+            // Si la celda llegó al límite y no es válido
+            if(matriz[ren][col] == 9){
+                setElemento(ren, col, 0);
+                return resuelve(prevRen(ren,col), prevCol(ren,col));
+            }
+            // Si no ha llegado a su límite
+            else {
+                setElemento(ren, col, matriz[ren][col]+1);
+                if (valida(ren, col))
+                    return resuelve(nextRen(ren,col), nextCol(ren,col));
+                else 
+                    return resuelve(ren, col);
+            }
+        }
+    }
+        
+    
+    
+
+    public static void main(String[] args) throws Exception{
+        /*NO sirve, falta revisar por qué*/
+        int[][] matriz = 
+                        {{5,3,0,0,7,0,0,0,0},
+                         {6,0,0,1,9,5,0,0,0},
+                         {0,9,8,0,0,0,0,6,0},
+                         {8,0,0,0,6,0,0,0,3},
+                         {4,0,0,8,0,3,0,0,1},
+                         {7,0,0,0,2,0,0,0,6},
+                         {0,6,0,0,0,0,2,8,0},
+                         {0,0,0,4,1,9,0,0,5},
+                         {0,0,0,0,8,0,0,7,9}};
+        //Sí sirve
+        int[][] matriz2 = 
+                        {{1,0,0,0,5,0,7,0,2},
+                         {0,0,0,6,0,3,0,0,8},
+                         {0,7,6,0,1,0,3,0,0},
+                         {0,4,0,3,0,7,0,5,0},
+                         {8,1,0,0,6,0,4,0,7},
+                         {0,9,0,0,0,5,0,0,0},
+                         {2,6,0,0,0,0,8,0,5},
+                         {9,0,0,8,0,1,0,0,0},
+                         {0,0,3,0,9,0,0,2,0}};
+        //Sí sirve
+        int[][] matriz3 = 
+                        {{0,0,0,0,0,1,2,3,0},
+                         {1,2,3,0,0,8,0,4,0},
+                         {8,0,4,0,0,7,6,5,0},
+                         {7,6,5,0,0,0,0,0,0},
+                         {0,0,0,0,0,0,0,0,0},
+                         {0,0,0,0,0,0,1,2,3},
+                         {0,1,2,3,0,0,8,0,4},
+                         {0,8,0,4,0,0,7,6,5},
+                         {0,7,6,5,0,0,0,0,0}};
+        Sudoku s = new Sudoku(matriz);
+        Sudoku s2 = new Sudoku(matriz2);
+        Sudoku s3 = new Sudoku(matriz3);
+//        System.out.println(s.toString());
+//        System.out.println(s.prevRen(4, 1));
+//        System.out.println(s.prevCol(4, 1));
+//        System.out.println(s.resuelve());
+        s3.resuelve();
+        System.out.println(s3.toString());
+    }
+    
+    
 }
